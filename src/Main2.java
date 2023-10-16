b84b7c61aa7cf3b97f7a52860df953f58c1f8e50
tatiSmol <judeclapton@icloud.com>
2023-10-16 21:34:26 +0300

    16.3 Exception types

--- src/Main2.java
+++ src/Main2.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main2 {
     public static void main(String[] args) {
         String[] names = {"Tatiana", "Julius", "Vasiliy"};
@@ -5,9 +10,19 @@ public class Main2 {
         try {
             System.out.println(names[5]);
         } catch (Exception ex) {
-            //System.out.println(ex.getMessage());
-            ex.printStackTrace();
+            System.out.println(ex.getMessage());
+//            ex.printStackTrace();
         }
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("file.txt"));
+            // проверяемое исключение, его необходимо обрабатывать
+        } catch (IOException e) {
+            //throw new RuntimeException(e);
+            System.out.println(e.getMessage());
+        }
+
+
         System.out.println("hey");
     }
 }
